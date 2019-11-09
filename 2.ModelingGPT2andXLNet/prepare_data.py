163a4a5a4ea46db68d8e829f81dfdd7fe8858040
tyu0912 <tennisonyu@dl-vm-tensorflow-1-vm.us-central1-c.c.w261-angela.internal>
2019-11-09 00:23:30 +0000

    changes to script for sentence level

--- 2.ModelingGPT2andXLNet/prepare_data.py
+++ 2.ModelingGPT2andXLNet/prepare_data.py
@@ -1,24 +1,45 @@
 import argparse
 
 
-
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--file", default=None, type=str, required=True, help="which speech?")
+    parser.add_argument("--level", default=None, type=str, required=True, help="which level?")
 
     args = parser.parse_args()
     in_path = '../1.DataPreparationResults/' + args.file 
-    out_path = 'processed_data/speech_level_train/' + args.file
-
+    
     fh = open(in_path).read().split('<speech_sep>')
-    out = open(out_path, 'w')
 
-    for sentence in fh:
-        sentence = sentence.strip()
-        sentence = '<sod> ' + sentence + '. <eod>\n'
-        out.write(sentence)
+    if args.level == 'speech':
+        out_path = 'processed_data/speech_level_train/' + args.file
+        out = open(out_path, 'w')
+
+        for sentence in fh:
+            sentence = sentence.strip()
+            sentence = '<sod> ' + sentence + '. <eod>\n'
+            out.write(sentence)
+                                                                                                            
+    elif args.level == 'sentence':
+        out_path = 'processed_data/sentence_level_train/' + args.file
+        out = open(out_path, 'w')
+
+        fh = list(map(lambda x: x.split('.'), fh))
+
+        fh = [item for sublist in fh for item in sublist] 
+
+        fh = list(map(lambda x: x.strip(), fh))
+
+        while "''" in fh:
+            fh = fh.remove('')
         
-
+        for i, sentence in enumerate(fh):
+            try:
+                sentence = '<sod> ' + sentence + '.' + fh[i+1] + '. <eod>\n'
+                out.write(sentence)
+            except:
+                pass
+            
 if __name__ == "__main__":
     main()
